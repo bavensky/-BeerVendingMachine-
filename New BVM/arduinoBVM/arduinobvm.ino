c0c4fff509968ff85a7f943c926e62ba67bf1119
CMMC\clicknectpc <apiruk_sang-ngenchai@hotmail.com>
2018-01-22 22:30:24 +0700

    update : add pinout and add httpGET

--- New BVM/arduinoBVM/arduinobvm.ino
+++ New BVM/arduinoBVM/arduinobvm.ino
@@ -1,35 +1,195 @@
+/*
+   Beer Vending Machine
+   RMUTL Senior Project
+   Date   : 27/11/2560
+   ---pin Connecting---
+
+    UART Arduino to ESP8266
+      TX      11    14
+      RX      10    12
+
+    RELAY  :
+      GND     GND
+      SIG     13
+
+    LCD i2c :
+      GND     GND
+      VCC     5V
+      SDA     04
+      SCL     05
+    SERVO :
+      GND     GND
+      VCC     5V
+      SIG     14
+    Flow sensor :
+      GND     12
+      VCC     GND
+      SDA     5V
+
+    Switch :
+      GND     GND
+      BT1     15
+      BT2     02
+      BT3     00
+      BT4     16
+*/
+
+
+#include <Wire.h>
+#include <Servo.h>
 #include <ArduinoJson.h>
+#include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 SoftwareSerial chat(10, 11); // RX, TX
 StaticJsonBuffer<200> jsonBuffer;
+JsonObject& master = jsonBuffer.createObject();
+Servo cup;
 
-int btn_230 = 0;
-int btn_300 = 0;
-int btn_400 = 0;
-int count230 = 0;
-int count300 = 0;
-int count400 = 0;
+#define FLOW 2
+
+#define CUP 8
+
+#define RELAY 9
+
+#define BT1 4
+#define BT2 5
+#define BT3 6
+#define BT4 7
+
+volatile int NbTopsFan;
+int Calc;
+
+int pos = 0;
+int flowLevel = 0;
+boolean state = false;
+
+byte beer1 = 0, beer2 = 0, beer3 = 0, beer4 = 0;
+
+
+
+void rpm () {
+  NbTopsFan++;
+}
+
+void calFlow() {
+  NbTopsFan = 0;
+  sei();
+  delay (1000);
+  cli();
+
+  //(Pulse frequency x 60) / 7.5Q, = flow rate in L / hour
+  Calc = (NbTopsFan * 60 / 7.5);
+  Serial.print(Calc, DEC);
+  Serial.println(" L/hour");
+}
+
+void sentData(int small, int medium, int large) {
+  JsonObject& data = jsonBuffer.createObject();
+  data["small"] = small;
+  data["medium"] = medium;
+  data["large"] = large;
+
+  //  JsonArray& data = master.createNestedArray("data");
+  //  data.add(small);
+  //  data.add(medium);
+  //  data.add(large);
+  Serial.print("createNestedArray : ");
+  master.printTo(Serial);
+  Serial.println("");
+  master.printTo(chat);
+  jsonBuffer.clear();
+}
 
 
 void setup()  {
   Serial.begin(9600);
   chat.begin(4800);
+  delay(1000);
+
+  lcd.begin();
+  lcd.backlight();
+
+  cup.attach(CUP);
+  //  cup.write(80);  // cup right
+  cup.write(110); // cup left
+
+  pinMode(BT1, INPUT_PULLUP);
+  pinMode(BT2, INPUT_PULLUP);
+  pinMode(BT3, INPUT_PULLUP);
+  pinMode(BT4, INPUT_PULLUP);
+
+  pinMode(FLOW, INPUT_PULLUP);
+  attachInterrupt(0, rpm, RISING);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Beer Vending Machine");
+  //  lcd.setCursor(0, 1);
+  //  lcd.print("                    ");
+  //  lcd.setCursor(0, 2);
+  //  lcd.print("                    ");
+  //  lcd.setCursor(0, 3);
+  //  lcd.print("                    ");
 }
 
 void loop() {
-  JsonObject& master = jsonBuffer.createObject();
-  JsonArray& data = master.createNestedArray("data");
-  Serial.print("createNestedArray : ");
-  data.add(count230);
-  data.add(count300);
-  data.add(count400);
-  master.printTo(Serial);
-  Serial.println("");
-  master.printTo(chat);
-  delay(2000);
-  count230++;
-  count300++;
-  count400++;
-  jsonBuffer.clear();
+  Serial.println("main...");
+  digitalWrite(RELAY, LOW);
+  if (digitalRead(BT4) == 0) {
+    delay(100);
+    beer1 += 1;
+    sentData(beer1, beer2, beer3);
+    Serial.println("button push");
+
+    //    cup.attach(CUP);
+    //    for (pos = 110; pos >= 80; pos -= 1) {
+    //      //      for (pos = 80; pos <= 110; pos += 1) {
+    //      cup.write(pos);
+    //      delay(50);
+    //    }
+
+    Serial.println("state true");
+    //    state = true;
+  }
+
+  while (state == true) {
+    Serial.println("water pump");
+    digitalWrite(RELAY, HIGH);
+    //    cup.write(110);
+    delay(2000);
+    //    for (int i = 0; i <= 9; i++) {
+    //      NbTopsFan = 0;
+    //      sei();
+    //      delay(1000);
+    //      cli();
+    //      Calc = (NbTopsFan * 60 / 7.5);
+    //      Serial.print(Calc, DEC);
+    //      Serial.println(" L/hour");
+    //    }
+    digitalWrite(RELAY, LOW);
+    //    for (pos = 110; pos >= 80; pos -= 1) {
+    for (pos = 80; pos <= 110; pos += 1) {
+      cup.write(pos);
+      delay(50);
+    }
+    cup.detach();
+    state = false;
+  }
+
+
+  // first display
+  lcd.setCursor(0, 0);
+  lcd.print("Beer Vending Machine");
+  lcd.setCursor(0, 1);
+  lcd.print("> 400 ml.           ");
+  lcd.setCursor(0, 2);
+  lcd.print("> Amount : ");
+  if (beer1 <= 9)  lcd.print("0");
+  lcd.print(beer1);
+  lcd.print("      ");
+  lcd.setCursor(0, 3);
+  lcd.print("                    ");
+  delay(200);
+
 }
--- New BVM/espBVM/espBVM.ino
+++ New BVM/espBVM/espBVM.ino
@@ -1,29 +1,98 @@
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
 
 SoftwareSerial chat(12, 14); // RX, TX
 StaticJsonBuffer<200> jsonBuffer;
+WiFiClient client;
+const char* ssid = "CMMC_Sinet_2.4G";
+const char* password = "zxc12345";
+String apiKey = "4QLHRBN0XRHRD4SJ";
+byte timeOutWIFI = 0;
+
+
+void doHttpGet(int Ibeer1, int Ibeer2, int Ibeer3) {
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+
+
+  http.begin("http://api.thingspeak.com/update?api_key=" + apiKey + "&field1=" + String(Ibeer1)
+             + "&field2=" + String(Ibeer2) + "&field3=" + String(Ibeer3)); //HTTP
+
+
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    Serial.print("[CONTENT]\n");
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+
+
 
 
 void setup() {
   Serial.begin(9600);
   chat.begin(4800);
   delay(10);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    timeOutWIFI += 1;
+    if (timeOutWIFI >= 20) {
+      timeOutWIFI = 0;
+    }
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
 }
 
 void loop() {
   JsonObject& slave = jsonBuffer.parseObject(chat);
-  int data1 = slave["data"][0];
-  int data2 = slave["data"][1];
-  double data3 =  slave["data"][2];
+  int small = slave["small"];
+  int medium = slave["medium"];
+  int large =  slave["large"];
+
+  //  int small = slave["data"][0];
+  //  int medium = slave["data"][1];
+  //  int large =  slave["data"][2];
 
   Serial.print("parseObject : ");
-  Serial.print(data1);
+  Serial.print(small);
   Serial.print("  ");
-  Serial.print(data2);
+  Serial.print(medium);
   Serial.print("  ");
-  Serial.println(data3, 6);
+  Serial.println(large);
   delay(500);
+
+  if (small != 0) {
+    doHttpGet(small, medium, large);
+  }
+
+
   jsonBuffer.clear();
+
+
 }
 
