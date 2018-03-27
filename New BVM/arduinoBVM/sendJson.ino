ee09f720133af2c358e377a8dc1b8fac141223e8
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-03-27 23:49:40 +0700

    complete Json parse

--- New BVM/arduinoBVM/sendJson.ino
+++ New BVM/arduinoBVM/sendJson.ino
@@ -1,17 +1,19 @@
 void sentData(int small, int medium, int large) {
-  //  JsonObject& data = jsonBuffer.createObject();
-  //  data["small"] = small;
-  //  data["medium"] = medium;
-  //  data["large"] = large;
+  JsonObject& data = jsonBuffer.createObject();
+  data["small"] = small;
+  data["medium"] = medium;
+  data["large"] = large;
 
-  JsonArray& data = master.createNestedArray("data");
-  data.add(small);
-  data.add(medium);
-  data.add(large);
+  //  JsonArray& data = master.createNestedArray("data");
+  //  data.add(small);
+  //  data.add(medium);
+  //  data.add(large);
 
   Serial.print("createNestedArray : ");
-  master.printTo(Serial);
+  //  master.printTo(Serial);
+  data.printTo(Serial);
   Serial.println("");
-  master.printTo(chat);
+  //  master.printTo(chat);
+  data.printTo(chat);
   jsonBuffer.clear();
 }
